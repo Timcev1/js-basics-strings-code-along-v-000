abbf460e4a65a21a2f851f3101dbe700aaecc280
Timothy Cevallos <timcev@gmail.com>
2017-10-05 17:15:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Timothy';
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}';
